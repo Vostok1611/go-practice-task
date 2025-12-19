247fb06e91cfc3b8edb4c55183c82e00e2a84b50
Daniyar <Danchik.ilich@mail.ru>
2025-12-19 10:27:28 +0500

    удалл автомигрейт

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	taskservice "gomeWork/internal/taskService"
 	"log"
 
 	"gorm.io/driver/postgres"
@@ -19,8 +18,5 @@ func InitDB() (*gorm.DB, error) {
 		log.Fatalf("Could not connect to database: %v", err)
 	}
 
-	if err := db.AutoMigrate(&taskservice.Task{}); err != nil {
-		log.Fatalf("Could not migrate: %v", err)
-	}
 	return db, nil
 }
